57506f016f18220c44d7ed39f831a0bd0acfce19
Tiago_Cruz <94022121+OBSCAtriox@users.noreply.github.com>
2025-10-05 14:44:34 +0100

    Add files via upload

---
+++ ProgramasTiago/Desafio3/Desafio3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Potencia(4, 3));
+            Console.ReadLine();
+
+        }
+
+        static int Potencia(int pownum, int basenum)
+        {
+            int i;
+            int result = 1;
+            for(i = 0; i<pownum; i++)
+            {
+                result = result + basenum;
+                 
+                 
+            }
+            return result;
+
+        }
+    }
+}
